c15560fd76e0d80ad0a7e94e851b1099e8b8605b
SIDDHARTHTAMSE <sidtamse7@gmail.com>
2026-06-12 12:49:29 +0530

    add arithmetic operators

---
+++ Module-03-Operators/arithmetic.py
@@ -0,0 +1,66 @@
+# Module 3: Operators in Python
+# What are Operators?
+
+# Operators are special symbols used to perform operations on values and variables.
+
+
+# Types of Operators in Python
+# Python has:
+
+# Arithmetic Operators
+# Comparison Operators
+# Logical Operators
+# Assignment Operators
+# Membership Operators
+# Identity Operators
+
+
+# 1. Arithmetic Operators
+# Used for mathematical calculations.
+
+# Addition (+)
+# example
+
+a = 10
+b = 5
+print(a+b)
+
+# Subtraction (-)
+a = 20
+b = 10
+print(a-b)
+
+# Multiplication (*)
+price = 100
+quantity = 5
+
+print(price*quantity)
+
+# Division (/)
+a = 10
+b = 2
+
+print(a/b)
+
+# Notice:
+# Division always gives float.
+
+# Modulus (%)
+# Gives remainder.
+
+print(10 % 3)
+
+
+# Floor Division (//)
+# Removes decimal part.
+print(10 // 3)
+
+
+# Power (**)
+# Used for exponent.
+
+# Example:
+print(2 ** 3)
+
+# Means:
+# 2 × 2 × 2
