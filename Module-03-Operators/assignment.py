de87bd92965d7eeaf1dabb53eac0b0c2726cfa89
SIDDHARTHTAMSE <sidtamse7@gmail.com>
2026-06-12 12:49:44 +0530

    add assignments operators

---
+++ Module-03-Operators/assignment.py
@@ -0,0 +1,33 @@
+# 4. Assignment Operators
+
+# Used to assign values.
+# =
+
+x = 10
+
+
+
+# +=
+x = 10
+
+x += 5
+
+print(x)
+
+# Same as:
+
+x = x + 5
+
+# Output:
+# 15
+
+
+
+# -=
+x = 10
+x -= 3
+
+print(x)
+
+# Output:
+# 7
