b8d399970029d1008fb9322819eb3d0fb7df6339
SIDDHARTHTAMSE <sidtamse7@gmail.com>
2026-06-12 12:50:18 +0530

    add logical operators

---
+++ Module-03-Operators/logical.py
@@ -0,0 +1,23 @@
+# 3. Logical Operators
+
+# Used to combine conditions.
+
+# and
+# Both conditions must be true.
+
+age = 25
+print(age > 15 and age < 50)
+
+
+# or
+# Any one condition true.
+
+marks = 80
+print( marks > 45 or marks > 90)
+
+
+# not
+# Reverse the result.
+
+is_login = True
+print(not is_login)
