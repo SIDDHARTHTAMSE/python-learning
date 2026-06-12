7f7fe67c73c8562f7b3e361c6a8abdb70810c483
SIDDHARTHTAMSE <sidtamse7@gmail.com>
2026-06-12 16:49:50 +0530

    add if statement

---
+++ Module-04-Conditional-Statements/01_if.py
@@ -0,0 +1,14 @@
+# 1. if Statement
+# What is if?
+
+# if executes a block of code only when the condition is True.
+
+# Syntax:
+
+# if condition:
+#     statement
+
+age = 27
+
+if age >= 18:
+    print("Adult")
