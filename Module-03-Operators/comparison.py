c57f543ce8bd143560b7d640bf96846540c0e2fa
SIDDHARTHTAMSE <sidtamse7@gmail.com>
2026-06-12 12:50:02 +0530

    add comparison operators

---
+++ Module-03-Operators/comparison.py
@@ -0,0 +1,35 @@
+# 2. Comparison Operators
+
+# Used to compare values.
+# They return:
+
+# True
+# False
+
+# Equal (==)
+a = 10
+print(a == 10)
+
+# Output:
+# True
+
+
+# Not Equal (!=)
+print(10!=5)
+
+# Output:
+# True
+
+
+# Greater Than (>)
+print(10>5)
+
+# Output:
+# True
+
+
+# Less Than (<)
+print(5 < 10)
+
+# Output:
+# True
