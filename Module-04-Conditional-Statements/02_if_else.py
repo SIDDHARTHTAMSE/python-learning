01f9d29e1f38b7ac069023ae349c6f1411a3982b
SIDDHARTHTAMSE <sidtamse7@gmail.com>
2026-06-12 16:50:38 +0530

    add if else conditional statement

---
+++ Module-04-Conditional-Statements/02_if_else.py
@@ -0,0 +1,34 @@
+# 2. if-else Statement
+# What is if-else?
+
+# When we have two choices:
+# If condition is True:
+# execute if block
+# If condition is False:
+# execute else block
+
+# Syntax:
+
+# if condition:
+#     statement
+# else:
+#     statement
+
+# Example
+
+age = 18
+
+if age >= 18:
+    print("Eligible for vote")
+else:
+    print("Not eligible")
+
+
+# Check Even or Odd
+
+number = 4
+
+if number % 2 == 0:
+    print("Number is even")
+else:
+    print("Number is odd")
